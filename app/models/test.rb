5e92941a46761d7de578bd78699aa631a32af5dd
timbynce <cherar90@gmail.com>
2022-05-18 23:29:54 +0300

    feat: New methods for Test and User. Added Result model.

---
+++ app/models/test.rb
@@ -0,0 +1,8 @@
+class Test < ApplicationRecord
+  def self.list_by_category(category)
+# как же уродливо выглядит этот запрос...
+# не уверен в том в каком виде требуется вернуть массив, поэтому для перестраховки сделал ему each
+    tests = Test.select('title').joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = ?", category).order('title DESC')
+    tests.each{|t| t.title}
+  end
+end
