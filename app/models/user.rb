872f6cf58cb2d8df03f426f6f4c002828658bc33
timbynce <cherar90@gmail.com>
2022-05-19 20:52:53 +0300

    fix: methods User and Test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  def results_by_level(level)
-    Result.joins("JOIN tests ON results.test_id = tests.id").where("results.user_id = ? AND tests.level = ?", level, id)
+  def tests_by_level(level)
+    Test
+      .joins('JOIN results ON results.test_id = tests.id')
+      .where("results.user_id = ? AND tests.level = ?", id, level)
   end
 end
