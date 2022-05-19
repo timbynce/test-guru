b327ee86c2fd3276314ac00daa58cba216a469a5
timbynce <cherar90@gmail.com>
2022-05-19 11:16:23 +0300

    feat: Added Methods to Test and User. Added Result model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def results_by_level(level)
+    Result.joins("JOIN tests ON results.test_id = tests.id").where("results.user_id = ? AND tests.level = ?", level, id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 Category.destroy_all
 User.destroy_all
 Test.destroy_all
@@ -42,3 +34,4 @@ Result.create(
   [{ passed: false, test_id: 3, user_id: 3 }, { passed: true, test_id: 2, user_id: 3 },
    { passed: false, test_id: 4, user_id: 3}  
  ])
+ 
