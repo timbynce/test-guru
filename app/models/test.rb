6094ddb7da0ef6763e7f84d075285b5e8468f033
timbynce <cherar90@gmail.com>
2022-05-20 13:55:25 +0300

    fix: Test model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   def self.list_by_category(title)
-    Test.joins("JOIN categories ON tests.category_id = categories.id")
-        .where("categories.title = ?", title)
-        .order('title DESC')
-        .pluck(:title)
+    joins("JOIN categories ON tests.category_id = categories.id")
+    .where("categories.title = ?", title)
+    .order('title DESC')
+    .pluck(:title)
   end
 end
