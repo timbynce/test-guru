ae38c542cf007c212c8e018036ffe1409880be6d
timbynce <cherar90@gmail.com>
2022-05-24 22:29:53 +0300

    fix: methods in models User, Tests. fix: seed

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,14 +4,15 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
 
-  has_many :questions
-  has_many :results
-  has_many :users, through: :results
+  has_many :questions, dependent: :destroy
+  has_many :results, dependent: :destroy
+  has_many :users, through: :results, dependent: :destroy
 
-  def self.list_by_category(title)
-    joins('JOIN categories ON tests.category_id = categories.id')
+  def self.list_by_category(title)  
+    joins(:category)
       .where(categories: { title: title })
       .order(title: :desc)
       .pluck(:title)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :results
-  has_many :tests, through: :results
-  has_many :tests_by_author, class_name: 'Test', foreign_key: :author_id
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
+  has_many :tests_by_author, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
   def tests_by_level(level)
-    Test.joins('JOIN results ON results.test_id = tests.id')
-        .where(results: { user_id: id }, tests: { level: level })
+    tests.where(level: level)
   end
+
 end
