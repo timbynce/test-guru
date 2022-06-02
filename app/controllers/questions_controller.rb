32abfb8e73ba7112d832cf9958e9eab7a96b5dd1
timbynce <cherar90@gmail.com>
2022-06-03 00:16:48 +0300

    fix and feat some feautures

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,26 +2,30 @@
 
 class QuestionsController < ApplicationController
   before_action :find_test
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    @questions = @test.questions
-    render html: { tests: @test.questions }
+    render plain: { tests: @test.questions }
   end
 
   def show
-    @question = @test.questions.find(params[:id])
-    render html: { tests: @question }
+    question = @test.questions.find(params[:id])
+
+    render plain: { test: question }
   end
 
   def new; end
 
   def create
-    @question = @test.questions.create(question_params)
-    render plain: @question.inspect
+    question = @test.questions.create(question_params)
+
+    render plain: question.inspect
   end
 
   def destroy
-    @question.destroy
+    question = @test.questions.find(params[:id])
+    question.destroy
+
     render plain: 'Question was successfully removed'
   end
 
@@ -33,5 +37,9 @@ class QuestionsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: "Question wasn't found"
   end
 end
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,20 +4,23 @@ class TestsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
   def index
-    @tests = Test.all
-    render html: { tests: @tests }
+    tests = Test.all
+    
+    render plain: { tests: tests }
   end
 
   def show
-    @test = Test.find(params[:id])
-    render html: { tests: @test }
+    test = Test.find(params[:id])
+
+    render plain: { tests: test }
   end
 
   def new; end
 
   def create
-    @test = Test.create(test_params)
-    render plain: @test.inspect
+    test = Test.create(test_params)
+
+    render plain: test.inspect
   end
 
   private
