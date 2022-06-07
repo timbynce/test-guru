83f6347945b6224e0f628b5d71297daba504d228
timbynce <cherar90@gmail.com>
2022-06-08 01:52:17 +0300

    feat:Lesson 8. Add forms, helpers and some more

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def current_year
+    Date.today.year
+  end
+
+  def github_url(author, repo)
+    link_to author, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module QuestionsHelper
+  def question_header(object, _name)
+    object.new_record? ? "Create new #{@test.title} question" : "Edit #{@question.test.title} question"
+  end
 end
