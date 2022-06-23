e4daee81350b090ab0c412c4f62293c02ab27657
timbynce <cherar90@gmail.com>
2022-06-24 01:14:46 +0300

    feat: locales for tests, admin, test-passages

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -2,9 +2,9 @@
 
 module TestsHelper
   def test_level(test)
-    return 'easy' if test.level <= 1
-    return 'hard' if test.level >= 5
+    return t('.easy') if test.level <= 1
+    return t('.hard') if test.level >= 5
 
-    'advanced'
+    t('.advanced')
   end
 end
