f30177e64c911a9310a1df42124a74a733a4925a
timbynce <cherar90@gmail.com>
2022-06-30 23:35:45 +0300

    screencast code

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TestGuru
 
     config.i18n.available_locales= %i(en ru)
     config.i18n.default_locale = :ru
+
+    config.autoload_paths << "#{Rails.root}/lib/clients"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
