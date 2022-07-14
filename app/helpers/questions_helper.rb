621cf564ddbb4c761a64a724178d99c1f17a186f
timbynce <cherar90@gmail.com>
2022-07-14 07:21:44 -0700

    feat localization

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,7 +3,7 @@
 module QuestionsHelper
   def question_header(question)
     if question.new_record?
-      "Create new #{question.test.title} question"
+      t(".new_question", test_name: question.test.title)
     else
       "Edit #{question.test.title} question"
     end
