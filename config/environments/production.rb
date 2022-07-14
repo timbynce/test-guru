12effecc2b26c1831c15e79c7c34273760d7d224
timbynce <cherar90@gmail.com>
2022-07-14 03:32:01 -0700

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'thawing-woodland-47654.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
+    address:              ENV["EMAIL_URL"],
+    port:                 465,
     user_name:            ENV['SMTP_USERNAME'],
     password:             ENV['SMTP_PASSWORD'],
     authentication:       'plain',
