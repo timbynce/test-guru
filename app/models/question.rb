67b545dab8d2c4f83e71933908281c1168d5b08e
timbynce <cherar90@gmail.com>
2022-07-18 14:56:54 -0700

    fix delete test when test_passage running and not finish

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,8 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
+  has_many :gists, dependent: :destroy
+  has_many :test_passages, class_name: 'TestPassage', foreign_key: :current_question_id, dependent: :destroy
 
   validates :body, presence: true
 
